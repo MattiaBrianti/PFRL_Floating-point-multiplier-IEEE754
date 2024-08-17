1455f6869f5ec5b9e0a822496f7fc62e8c86feff
AlexIlLeone2 <alex.hathaway@mail.polimi.it>
2024-08-17 13:14:57 +0200

    expanded exp_adder

--- EXP_ADDER.vhd
+++ EXP_ADDER.vhd
@@ -10,6 +10,18 @@ entity EXP_ADDER is
         SUM : out STD_LOGIC_VECTOR (8 downto 0);
     );
 
-    
+architecture RTL of EXP_ADDER is
+    signal CARRY : STD_LOGIC_VECTOR (7 downto 0);
+    signal SUM_TEMP : STD_LOGIC_VECTOR (8 downto 0);
+
+    component CSA is
+        generic (N : INTEGER := 8);
+        port (
+            X : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            Y : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            Z : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            S : out STD_LOGIC_VECTOR (N + 1 downto 0)
+        );
+    end component;
 
 end EXP_ADDER;
