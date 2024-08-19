beee2b0768ff6ab130d8566e9f124dd913120fae
MattiaBrianti <Mattia.brianti@mail.polimi.it>
2024-08-19 22:37:41 +0200

    Started Bias_Subtractor

--- BIAS_SUBTRACTOR.vhd
+++ BIAS_SUBTRACTOR.vhd
@@ -1,18 +1,35 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity BIAS_SUBTRACTOR is
-    generic (
-        BIAS : integer := 127
-    );
     port (
-        exp_sum : in  integer range -127 to 128;
-        exp_out : out integer range -127 to 128
+        exp_sum : in STD_LOGIC_VECTOR(8 downto 0);
+        exp_out : out STD_LOGIC_VECTOR(9 downto 0)
     );
 end entity BIAS_SUBTRACTOR;
 
 architecture RTL of BIAS_SUBTRACTOR is
+    component CSA is
+
+        generic (N : INTEGER := 32);
+        port (
+            X : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            Y : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            Z : in STD_LOGIC_VECTOR (N - 1 downto 0);
+            S : out STD_LOGIC_VECTOR (N + 1 downto 0)
+        );
+    end component CSA;
+
 begin
-    exp_out <= exp_sum - BIAS;
+    U1 : CSA
+    generic map(N => 9)
+    port map(
+        X => exp_sum(N - 1 downto 0),
+        Y => "10000001",
+        Z => "00000000",
+        S => exp_out(N downto 0)
+    );
 end architecture RTL;
+
+-- Non funziona il test
+-- Capire a cosa serve il MSB, usato dopo per il segno (?)
