532c93c0b38a6675aa6a906a0bfbc8d2f14da179
MattiaBrianti <Mattia.brianti@mail.polimi.it>
2024-08-25 15:10:58 +0200

    MANTIX_MULTIPLIER Updates

--- MANTIX_MULTIPLIER.vhdl
+++ MANTIX_MULTIPLIER.vhdl
@@ -18,27 +18,29 @@ architecture RTL of MANTIX_MULTIPLIER is
         );
         port (
             X, Y, Z : in STD_LOGIC_VECTOR(46 downto 0);
-            S : out STD_LOGIC_VECTOR(47 downto 0)
+            S : out STD_LOGIC_VECTOR(48 downto 0)
         );
     end component CSA;
 
-    type partial_product_array is array (0 to N - 1) of STD_LOGIC_VECTOR(2 * N - 1 downto 0); --array of 24 partial products of 48 bits
+    type partial_product_array is array (0 to 23) of STD_LOGIC_VECTOR(46 downto 0); --array of 24 partial products of 48 bits
     signal partial_products : partial_product_array;
     --signal S : STD_LOGIC_VECTOR(2 * N - 1 downto 0); --sum of partial products
-    type partial_sum_1_array is array (0 to 7) of STD_LOGIC_VECTOR(2 * N downto 0); --array of 8 partial sums of 48 bits
+    type partial_sum_1_array is array (0 to 7) of STD_LOGIC_VECTOR(48 downto 0); --array of 8 partial sums of 48 bits
     signal partial_sum_1 : partial_sum_1_array;
-    type partial_sum_2_array is array (0 to 3) of STD_LOGIC_VECTOR(2 * N  downto 0); --array of 8 partial sums of 48 bits
+    type partial_sum_2_array is array (0 to 3) of STD_LOGIC_VECTOR(48 downto 0); --array of 8 partial sums of 48 bits
     signal partial_sum_2 : partial_sum_2_array;
     signal Cout : STD_LOGIC;
 begin
-    gen_partial_products : for i in 0 to 23 generate
-    gen_if : if (i = 0) generate
-        partial_products(i) <= (others => '0') when B(i) = '0' else (46 downto 24 + i => '0') & A; -- For i=0, no zeros on the right
-    end generate;
-    gen_else : if (i /= 0) generate
-        partial_products(i) <= (46 downto 24 + i => '0') & A & (i - 1 downto 0 => '0');
-    end generate;
-end generate gen_partial_products;
+    partial_products(0) <= (others => '0') when B(0) = '0' else
+    "00000000000000000000000" & A; -- For i=0, no zeros on the right
+    partial_products(23) <= (others => '0') when B(23) = '0' else
+    A & "00000000000000000000000"; --Last case
+
+    gen_partial_products : for i in 1 to 23 generate
+        partial_products(i) <= (others => '0') when B(i) = '0' else
+        (23 - i => '0') & A & (i => '0');
+    end generate gen_partial_products;
+
     -- Because a CSA sum three numbers at a time, we'll need 8 CSA to sum 24 numbers
     GEN_CSA : for I in 0 to 7 generate
         CSA_1 : CSA
@@ -51,11 +53,11 @@ end generate gen_partial_products;
         );
     end generate GEN_CSA;
 
-    partial_sum_1(7) <= "00000000000000000000000000000000000000000000000"; --initialize the last partial sum to 48 zeros
+    partial_sum_1(7) <= "0000000000000000000000000000000000000000000000"; --initialize the last partial sum to 47 zeros
 
     GEN_CSA_2 : for J in 0 to 2 generate
         CSA_2 : CSA
-        generic map(N => 47)
+        generic map(N => 49)
         port map(
             X => partial_sum_1(J * 3),
             Y => partial_sum_1(J * 3 + 1),
