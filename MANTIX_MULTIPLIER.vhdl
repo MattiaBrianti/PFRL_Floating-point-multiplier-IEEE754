0cb6ea0dc12933bb357ba983d8127e83eae74d55
AlexHatha <alexhath@gmail.com>
2024-08-26 15:07:00 +0200

    fixed MM, need to simulate

--- MANTIX_MULTIPLIER.vhdl
+++ MANTIX_MULTIPLIER.vhdl
@@ -17,8 +17,8 @@ architecture RTL of MANTIX_MULTIPLIER is
             N : POSITIVE
         );
         port (
-            X, Y, Z : in STD_LOGIC_VECTOR(46 downto 0);
-            S : out STD_LOGIC_VECTOR(48 downto 0)
+            X, Y, Z : in STD_LOGIC_VECTOR(N-1 downto 0);
+            S : out STD_LOGIC_VECTOR(N+1 downto 0)
         );
     end component CSA;
 
@@ -31,18 +31,20 @@ architecture RTL of MANTIX_MULTIPLIER is
 	 type partial_sum_1_array_fixed is array (0 to 8) of STD_LOGIC_VECTOR(47 downto 0); --array of 8 partial sums of 49 bits
     signal partial_sum_1_fixed : partial_sum_1_array_fixed;
 	 
-    type partial_sum_2_array is array (0 to 2) of STD_LOGIC_VECTOR(48 downto 0); --array of 3 partial sums of 49 bits
+    type partial_sum_2_array is array (0 to 2) of STD_LOGIC_VECTOR(49 downto 0); --array of 3 partial sums of 49 bits
     signal partial_sum_2 : partial_sum_2_array;
 	 
 	 type partial_sum_2_array_fixed is array (0 to 8) of STD_LOGIC_VECTOR(47 downto 0); --array of 8 partial sums of 49 bits
     signal partial_sum_2_fixed : partial_sum_2_array_fixed;
+	 
+	 signal P_TEMP: std_logic_vector(49 downto 0);
     
 	 
 begin
     partial_products(0) <= (others => '0') when B(0) = '0' else
     "00000000000000000000000" & A; -- For i=0, no zeros on the right
     partial_products(23) <= (others => '0') when B(23) = '0' else
-    A & "00000000000000000000000"; --Last case
+    A & "00000000000000000000000"; --Last case, no zeros on the left
 
     gen_partial_products : for i in 1 to 23 generate
         partial_products(i) <= (others => '0') when B(i) = '0' else
@@ -52,7 +54,7 @@ begin
     -- Because a CSA sum three numbers at a time, we'll need 8 CSA to sum 24 numbers
     GEN_CSA : for I in 0 to 7 generate
         CSA_1 : CSA
-        generic map(N => 47) -- il problema 'e che qua sto dicendo che s sara di 50 bit, come da specifica del CSA
+        generic map(N => 47) 
         port map(
             X => partial_products(I * 3),
             Y => partial_products(I * 3 + 1),
@@ -89,7 +91,10 @@ begin
         X => partial_sum_2_fixed(0),
         Y => partial_sum_2_fixed(1),
         Z => partial_sum_2_fixed(2),
-        S => P
+        S => P_TEMP
     );
+	 
+	 P<= P_TEMP(47 downto 0);
+	 
 
 end architecture RTL;
