f5d6f1a9dd03be6f7c9f83be75001568d2909fed
AlexHatha <alexhath@gmail.com>
2024-08-28 12:11:31 +0200

    csa_tb

--- TB_CSA.vhd
+++ TB_CSA.vhd
@@ -9,28 +9,29 @@ ARCHITECTURE behavior OF TB_CSA IS
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT CSA
+	 generic (N : integer := 48);
     PORT(
 	         X : in STD_LOGIC_VECTOR (N - 1 downto 0);
             Y : in STD_LOGIC_VECTOR (N - 1 downto 0);
             Z : in STD_LOGIC_VECTOR (N - 1 downto 0);
-            S : out STD_LOGIC_VECTOR (N downto 0)
+            S : out STD_LOGIC_VECTOR (N+1 downto 0)
         );
     END COMPONENT;
 	 --inputs
-	         signal X : STD_LOGIC_VECTOR (N - 1 downto 0);
-            signal Y : STD_LOGIC_VECTOR (N - 1 downto 0);
-            signal Z : STD_LOGIC_VECTOR (N - 1 downto 0);
+	 signal X : STD_LOGIC_VECTOR (47 downto 0);
+    signal Y : STD_LOGIC_VECTOR (47 downto 0);
+    signal Z : STD_LOGIC_VECTOR (47 downto 0);
 	--outputs
-	         signal S: STD_LOGIC_VECTOR(N downto 0);
+	 signal S: STD_LOGIC_VECTOR(49 downto 0);
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: CSA PORT MAP (
-	X => X;
-	Y=> Y;
-	Z=> Z;
-	S=> S;
+	X => X,
+	Y=> Y,
+	Z=> Z,
+	S=> S
         );
 
    
@@ -50,4 +51,5 @@ BEGIN
     Y <= "100000000000000000000000000000000000000000010000";
     Z <= "100000100000000000000000000000000000000000000001";
     wait for 20 ns;
+	 end process;
 END;
