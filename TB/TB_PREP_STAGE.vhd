3d24a2d58e9b1c50d9aafa6b6dceda64e5cf0cc6
MattiaBrianti <Mattia.brianti@mail.polimi.it>
2024-08-29 19:59:37 +0200

    TB_PREP STAGE FIXED

--- TB/TB_PREP_STAGE.vhd
+++ TB/TB_PREP_STAGE.vhd
@@ -1,98 +1,91 @@
+
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity PREP_STAGE is
-   port (
-      X : in STD_LOGIC_VECTOR(31 downto 0);
-      Y : in STD_LOGIC_VECTOR(31 downto 0);
-      FLAG : out STD_LOGIC_VECTOR (1 downto 0); -- 2-bit flag for INF, NAN, ZERO and DENORM
-      S : out STD_LOGIC;
-      EXP_X : out STD_LOGIC_VECTOR (7 downto 0);
-      EXP_Y : out STD_LOGIC_VECTOR (7 downto 0);
-      FIXED_MANT_X : out STD_LOGIC_VECTOR (23 downto 0); -- 24-bit mantissa
-      FIXED_MANT_Y : out STD_LOGIC_VECTOR (23 downto 0) -- 24-bit mantissa
-   );
-end entity PREP_STAGE;
+entity TB_PREP_STAGE is
+end TB_PREP_STAGE;
 
-architecture RTL of PREP_STAGE is
+architecture behavior of TB_PREP_STAGE is
 
-   signal S_X : STD_LOGIC;
-   signal S_Y : STD_LOGIC;
-   signal MANT_X : STD_LOGIC_VECTOR(22 downto 0);
-   signal MANT_Y : STD_LOGIC_VECTOR(22 downto 0);
-   signal S_EXP_X : STD_LOGIC_VECTOR(7 downto 0);
-   signal S_EXP_Y : STD_LOGIC_VECTOR(7 downto 0);
+   -- Component Declaration for the Unit Under Test (UUT)
 
-   component EDGE_CASES_CHECK is
+   component PREP_STAGE
       port (
-         EXP_X : in STD_LOGIC_VECTOR (7 downto 0); -- 8-bit exponent
-         EXP_Y : in STD_LOGIC_VECTOR (7 downto 0); -- 8-bit exponent
-         MANT_X : in STD_LOGIC_VECTOR (22 downto 0); -- 23-bit mantissa
-         MANT_Y : in STD_LOGIC_VECTOR (22 downto 0); -- 23-bit mantissa
-         FLAG : out STD_LOGIC_VECTOR (1 downto 0) -- 2-bit flag for INF, NAN, ZERO and DENORM
+         X : in STD_LOGIC_VECTOR(31 downto 0);
+         Y : in STD_LOGIC_VECTOR(31 downto 0);
+         FLAG : out STD_LOGIC_VECTOR(1 downto 0);
+         S : out STD_LOGIC;
+         EXP_X : out STD_LOGIC_VECTOR(7 downto 0);
+         EXP_Y : out STD_LOGIC_VECTOR(7 downto 0);
+         FIXED_MANT_X : out STD_LOGIC_VECTOR(23 downto 0);
+         FIXED_MANT_Y : out STD_LOGIC_VECTOR(23 downto 0)
       );
    end component;
+   --Inputs
+   signal X : STD_LOGIC_VECTOR(31 downto 0);
+   signal Y : STD_LOGIC_VECTOR(31 downto 0);
 
-   component OPERANDS_SPLITTER is
-      port (
-         X : in STD_LOGIC_VECTOR (31 downto 0);
-         Y : in STD_LOGIC_VECTOR (31 downto 0);
-         S_X : out STD_LOGIC;
-         S_Y : out STD_LOGIC;
-         EXP_X : out STD_LOGIC_VECTOR (7 downto 0);
-         EXP_Y : out STD_LOGIC_VECTOR (7 downto 0);
-         MANT_X : out STD_LOGIC_VECTOR (22 downto 0);
-         MANT_Y : out STD_LOGIC_VECTOR (22 downto 0)
-      );
-   end component;
-
-   component MANTIX_FIXER is
-      port (
-         MANT_X : in STD_LOGIC_VECTOR (22 downto 0); -- 23-bit mantissa
-         MANT_Y : in STD_LOGIC_VECTOR (22 downto 0); -- 23-bit mantissa
-         EXP_X : in STD_LOGIC_VECTOR (7 downto 0); -- 8-bit exponent
-         EXP_Y : in STD_LOGIC_VECTOR (7 downto 0); -- 8-bit exponent
-
-         FIXED_MANT_X : out STD_LOGIC_VECTOR (23 downto 0); -- 24-bit mantissa
-         FIXED_MANT_Y : out STD_LOGIC_VECTOR (23 downto 0) -- 24-bit mantissa
-      );
-   end component;
-
+   --Outputs
+   signal FLAG : STD_LOGIC_VECTOR(1 downto 0);
+   signal S : STD_LOGIC;
+   signal EXP_X : STD_LOGIC_VECTOR(7 downto 0);
+   signal EXP_Y : STD_LOGIC_VECTOR(7 downto 0);
+   signal FIXED_MANT_X : STD_LOGIC_VECTOR(23 downto 0);
+   signal FIXED_MANT_Y : STD_LOGIC_VECTOR(23 downto 0);
 begin
 
-   U1 : OPERANDS_SPLITTER
-   port map(
+   -- Instantiate the Unit Under Test (UUT)
+   uut : PREP_STAGE port map(
       X => X,
       Y => Y,
-      S_X => S_X,
-      S_Y => S_Y,
-      EXP_X => S_EXP_X,
-      EXP_Y => S_EXP_Y,
-      MANT_X => MANT_X,
-      MANT_Y => MANT_Y
-   );
-
-   U2 : MANTIX_FIXER
-   port map(
-      MANT_X => MANT_X,
-      MANT_Y => MANT_Y,
-      EXP_X => S_EXP_X,
-      EXP_Y => S_EXP_Y,
+      FLAG => FLAG,
+      S => S,
+      EXP_X => EXP_X,
+      EXP_Y => EXP_Y,
       FIXED_MANT_X => FIXED_MANT_X,
       FIXED_MANT_Y => FIXED_MANT_Y
    );
 
-   U3 : EDGE_CASES_CHECK
-   port map(
-      EXP_X => S_EXP_X,
-      EXP_Y => S_EXP_Y,
-      MANT_X => MANT_X,
-      MANT_Y => MANT_Y,
-      FLAG => FLAG
-   );
+   -- Stimulus process
+   process
+   begin
+      -- EDGE: ZERO
+      X <= "00000000000000000000000000000000";
+      Y <= "10011100100101000011010001111011";
+      wait for 20 ns;
 
-   S <= S_X xor S_Y;
-   EXP_X <= S_EXP_X;
-   EXP_Y <= S_EXP_Y;
+      -- EDGE: INF
+      X <= "01111111100000000000000000000000";
+      Y <= "10011100111110100001101000111101";
+      wait for 20 ns;
 
-end RTL;
+      -- EDGE: NAN
+      X <= "01111101100000000001000000010000";
+      Y <= "01111111100000000001000000000000";
+      wait for 20 ns;
+      -- EDGE: BOTH DENORM
+      X <= "00000000000000000101000000000000";
+      Y <= "00000000000000000101000000110000";
+      wait for 20 ns;
+
+      -- EDGE: BOTH DENORM
+      X <= "00000000000000000101000000000000";
+      Y <= "00000000000000000101000000110000";
+      wait for 20 ns;
+
+      -- NEG*POS - 
+      X <= "10000000000000000101000000000000"; --DENORM NEG
+      Y <= "00100100000000000101000000000000"; -- NORM POS
+      wait for 20 ns;
+
+      -- NEG*NEG - 
+      X <= "10000000000000000101000000000000"; --DENORM NEG
+      Y <= "10100100000000000101000000000000"; -- NORM NEG
+      wait;
+
+      -- 1*1
+      X <= "00111111100000000000000000000000";
+      Y <= "00111111100000000000000000000000";
+   end process;
+
+end;
